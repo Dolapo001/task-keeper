3406c7ab59610b8afabfd7d315310bde4caeb746
Dolapo24 <adedolapovictoria927@gmail.com>
2023-01-23 13:44:31 +0100

    working on a to do app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_list.html"
+
+
+class TaskDetailView(DetailView): # new
+    model = Task
+    template_name = "task_detail.html"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Task
+    fields = (
+        "title",
+        "body",
+    )
+    template_name = "task_edit.html"
+
+
+class TaskDeleteView(DeleteView): # new
+    model = Task
+    template_name = "task_delete.html"
+    success_url = reverse_lazy("task_list")
